989743323fc238a7c23ece3fba81cb42cbf3a196
MaryBorges <marianamarianoborges>
2022-02-07 18:57:35 -0300

    function add new city in my list

--- src/screens/Seach/index.tsx
+++ src/screens/Seach/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { useTheme } from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 
@@ -17,6 +19,7 @@ import { CityCard } from "../../components/CityCard";
 import { Load } from "../../components/Load";
 
 import apiMapBox from "../../services/apiMapBox";
+import { Alert } from "react-native";
 
 const TOKEN = 'access_token=pk.eyJ1IjoibWFyaWFuYW1ib3JnZXMiLCJhIjoiY2t6Y3R4MXh0Mm9lNjJ2cDRvcGo0ODJsaiJ9.hCa1jQvoYhIIHhyY9JmW6Q'
 
@@ -82,26 +85,49 @@ export function Seach(){
             setIsLoading(true);
             const response = await apiMapBox.get(`${city}.json?types=place&${TOKEN}`);
             const data = response.data.features;
-            const dataFormatted: CityProps[] = data.map((item:ResponseMapBoxProps)=>{
-                const newItem = {
-                    id: item.id,
-                    estate: item.context[0].text,
-                    country: item.context[1].text,
-                    city: item.text,
-                    latitude: item.geometry.coordinates[1],
-                    logitude: item.geometry.coordinates[0],
-                    isFavorite: false,
-                }
-                return newItem;
-            })
+            const dataFormatted: CityProps[] = data
+                .map((item:ResponseMapBoxProps)=>{
+                    const newItem = {
+                        id: item.id,
+                        estate: item.context[0].text,
+                        country: item.context[1].text,
+                        city: item.text,
+                        latitude: item.geometry.coordinates[1],
+                        logitude: item.geometry.coordinates[0],
+                        isFavorite: false,
+                    }
+                    return newItem;
+                })
 
-            console.log('dataFormatted',dataFormatted);
+                //console.log(dataFormatted)
             setCities(dataFormatted);
 
         } catch (error) {
             console.log(error);
         } finally{
             setIsLoading(false);
+        }
+    }
+
+    async function handleNewCity(id: string){
+        const city = cities.find(city => city.id === id);
+
+        if(city){
+            const response = await AsyncStorage.getItem('@RN_weatherForecastApp');
+            const data = response ? JSON.parse(response) : [];
+
+            const verifyCity = data.find( (cityStorage: CityProps) => cityStorage.id === id )
+            console.log('verifyCity', !verifyCity);
+            if(verifyCity){
+                return Alert.alert('Oops!', 'Você já adicionou essa cidade!')
+            }
+
+            data.push(city);
+
+            await AsyncStorage.setItem('@RN_weatherForecastApp', JSON.stringify(data));
+            Alert.alert('Legal!',"Cidade adicionada a sua lista!");
+        }else{
+            Alert.alert('Oops!',"Algo deu errado");
         }
     }
 
@@ -130,17 +156,14 @@ export function Seach(){
                     : <CitiesList
                         data={cities}
                         keyExtractor={(item) => item.id}
-                        renderItem={({item})=> <CityCard data={item}/>}
+                        renderItem={({item})=> 
+                            <CityCard 
+                                data={item}
+                                onPress={() => handleNewCity(item.id)}
+                            />}
                         />
                 }
             </Content>
         </Container>
     )
 }
-
-/*
-  {
-    
-  }
-]
-*/
